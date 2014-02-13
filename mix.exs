65eac2790e50660687e10a12fe15004b88b8ce72
Michael Weibel <michael.weibel@gmail.com>
2014-02-13 19:07:21 +0100

    Project skeleton

---
+++ mix.exs
@@ -0,0 +1,30 @@
+defmodule Facebook.Mixfile do
+	use Mix.Project
+
+	def project do
+		[ app: :facebook,
+			version: "0.0.1",
+			elixir: "~> 0.12.4",
+			deps: deps ]
+	end
+
+	# Configuration for the OTP application
+	def application do
+		[
+			mod: { Facebook, [] },
+			applications: [:json]
+		]
+	end
+
+	# Returns the list of dependencies in the format:
+	# { :foobar, git: "https://github.com/elixir-lang/foobar.git", tag: "0.1" }
+	#
+	# To specify particular versions, regardless of the tag, do:
+	# { :barbat, "~> 0.1", github: "elixir-lang/barbat" }
+	defp deps do
+		[
+			{:json, "~>0.2.7", [github: "cblage/elixir-json", tag: "v0.2.7"]},
+			{:hackney, "~>0.9.1", [github: "benoitc/hackney", tag: "0.9.1"]}
+		]
+	end
+end
---
+++ test/facebook_test.exs
@@ -0,0 +1,7 @@
+defmodule FacebookTest do
+	use ExUnit.Case
+
+	test "the truth" do
+		assert(true)
+	end
+end
