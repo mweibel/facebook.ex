5f1ab95b37d35a7220f8d0cec10e7da5a2b01dc8
Michael Weibel <michael.weibel@gmail.com>
2014-02-21 18:42:51 +0100

    /me/likes API

--- lib/facebook.ex
+++ lib/facebook.ex
@@ -4,12 +4,22 @@ defmodule Facebook do
 	end
 
 	def me(fields, access_token) do
-		me(fields, access_token, nil)
+		me(fields, access_token, [])
 	end
 
 	def me(fields, access_token, options) do
 		Facebook.Graph.get("/me", [
 			{<<"fields">>, fields},
+			{<<"access_token">>, access_token}
+		], options)
+	end
+
+	def myLikes(access_token) do
+		myLikes(access_token, [])
+	end
+
+	def myLikes(access_token, options) do
+		Facebook.Graph.get("/me/likes", [
 			{<<"access_token">>, access_token}
 		], options)
 	end
