3e2b92bf085e9d5067009f26efe3518f65574611
Michael Weibel <michael.weibel@gmail.com>
2014-04-22 07:40:30 +0200

    Bump package.exs version

--- package.exs
+++ package.exs
@@ -1,9 +1,9 @@
 Expm.Package.new(
 	name: "facebook",
 	description: "Facebook Graph API Wrapper",
-	version: "0.0.2",
+	version: "0.0.3",
 	licenses: [[name: "MIT"]],
 	keywords: ["facebook", "graph", "oauth", "api"],
 	maintainers: [[name: "Michael Weibel", email: "michael.weibel@gmail.com"]],
-	repositories: [[github: "mweibel/facebook.ex", tag: "0.0.2"]]
+	repositories: [[github: "mweibel/facebook.ex", tag: "0.0.3"]]
 )
