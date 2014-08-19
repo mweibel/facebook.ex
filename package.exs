49103e9a011a9a0f9cdcd3eed3696dd70af11f6c
Michael Weibel <michael.weibel@gmail.com>
2014-08-19 07:44:06 +0200

    Remove expm package.exs file

--- package.exs
+++
@@ -1,9 +0,0 @@
-Expm.Package.new(
-	name: "facebook",
-	description: "Facebook Graph API Wrapper",
-	version: "0.1.0",
-	licenses: [[name: "MIT"]],
-	keywords: ["facebook", "graph", "oauth", "api"],
-	maintainers: [[name: "Michael Weibel", email: "michael.weibel@gmail.com"]],
-	repositories: [[github: "mweibel/facebook.ex", tag: "0.0.3"]]
-)
