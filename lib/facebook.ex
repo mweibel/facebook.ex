77e17f85113e471a853bfa64c989ac82a0273753
Héctor Iván <hectorivanpatriciomoreno@gmail.com>
2016-01-26 21:05:42 -0600

    Updating version of json package, with a resolved bug

--- lib/facebook.ex
+++ lib/facebook.ex
@@ -1,170 +1,170 @@
 defmodule Facebook do
-	use Application
-	use Supervisor
+  use Application
+  use Supervisor
 
-	@moduledoc """
-	Provides API wrappers for the Facebook Graph API
+  @moduledoc """
+  Provides API wrappers for the Facebook Graph API
 
-	See: https://developers.facebook.com/docs/graph-api
-	"""
+  See: https://developers.facebook.com/docs/graph-api
+  """
 
-	alias Facebook.Config
+  alias Facebook.Config
 
-	@doc "Start hook"
-	def start(_type, _args) do
-		start_link([])
-	end
+  @doc "Start hook"
+  def start(_type, _args) do
+    start_link([])
+  end
 
-	@doc "Supervisor start"
-	def start_link(_) do
-		Supervisor.start_link(__MODULE__, [])
-	end
+  @doc "Supervisor start"
+  def start_link(_) do
+    Supervisor.start_link(__MODULE__, [])
+  end
 
-	def init(_) do
-		children = [
-			worker(Facebook.Graph, [])
-		]
+  def init(_) do
+    children = [
+      worker(Facebook.Graph, [])
+    ]
 
-		supervise(children, strategy: :one_for_one)
-	end
+    supervise(children, strategy: :one_for_one)
+  end
 
-	@type fields :: list
-	@type access_token :: String.t
-	@type response :: {:json, HashDict.t} | {:body, String.t}
-	@type options :: list
-	@type using_appsecret :: boolean
+  @type fields :: list
+  @type access_token :: String.t
+  @type response :: {:json, HashDict.t} | {:body, String.t}
+  @type options :: list
+  @type using_appsecret :: boolean
 
-	@doc """
-	If you want to use an appsecret proof, pass it into set_appsecret:
-	Facebook.set_appsecret("appsecret")
+  @doc """
+  If you want to use an appsecret proof, pass it into set_appsecret:
+  Facebook.set_appsecret("appsecret")
 
-	See: https://developers.facebook.com/docs/graph-api/securing-requests
-	"""
-	def set_appsecret(appsecret) do
-		Config.appsecret(appsecret)
-	end
+  See: https://developers.facebook.com/docs/graph-api/securing-requests
+  """
+  def set_appsecret(appsecret) do
+    Config.appsecret(appsecret)
+  end
 
-	@doc """
-	Basic user infos of the logged in user (specified by the access_token)
+  @doc """
+  Basic user infos of the logged in user (specified by the access_token)
 
-	See: https://developers.facebook.com/docs/graph-api/reference/user/
-	"""
-	@spec me(fields, access_token) :: response
-	def me(fields, access_token) when is_binary(fields) do
-		me([fields: fields], access_token, [])
-	end
+  See: https://developers.facebook.com/docs/graph-api/reference/user/
+  """
+  @spec me(fields, access_token) :: response
+  def me(fields, access_token) when is_binary(fields) do
+    me([fields: fields], access_token, [])
+  end
 
-	def me(fields, access_token) do
-		me(fields, access_token, [])
-	end
+  def me(fields, access_token) do
+    me(fields, access_token, [])
+  end
 
-	@doc """
-	Basic user infos of the logged in user (specified by the access_token).
+  @doc """
+  Basic user infos of the logged in user (specified by the access_token).
 
-	See: https://developers.facebook.com/docs/graph-api/reference/user/
-	"""
-	@spec me(fields :: String.t, access_token, options) :: response
-	def me(fields, access_token, options) when is_binary(fields) do
-		me([fields: fields], access_token, options)
-	end
+  See: https://developers.facebook.com/docs/graph-api/reference/user/
+  """
+  @spec me(fields :: String.t, access_token, options) :: response
+  def me(fields, access_token, options) when is_binary(fields) do
+    me([fields: fields], access_token, options)
+  end
 
-	@doc """
-	Basic user infos of the logged in user (specified by the access_token).
+  @doc """
+  Basic user infos of the logged in user (specified by the access_token).
 
-	See: https://developers.facebook.com/docs/graph-api/reference/user/
-	"""
-	@spec me(fields, access_token, options) :: response
-	def me(fields, access_token, options) do
-		if !is_nil(Config.appsecret) do
-			fields = fields ++ [appsecret_proof: encrypt(access_token)]
-		end
+  See: https://developers.facebook.com/docs/graph-api/reference/user/
+  """
+  @spec me(fields, access_token, options) :: response
+  def me(fields, access_token, options) do
+    if !is_nil(Config.appsecret) do
+      fields = fields ++ [appsecret_proof: encrypt(access_token)]
+    end
 
-		Facebook.Graph.get("/me", fields ++ [access_token: access_token], options)
-	end
+    Facebook.Graph.get("/me", fields ++ [access_token: access_token], options)
+  end
 
-	@doc """
-	Likes of the currently logged in user (specified by the access_token)
+  @doc """
+  Likes of the currently logged in user (specified by the access_token)
 
-	See: https://developers.facebook.com/docs/graph-api/reference/user/likes
-	"""
-	@spec myLikes(access_token) :: response
-	def myLikes(access_token) do
-		myLikes(access_token, [])
-	end
+  See: https://developers.facebook.com/docs/graph-api/reference/user/likes
+  """
+  @spec myLikes(access_token) :: response
+  def myLikes(access_token) do
+    myLikes(access_token, [])
+  end
 
-	@doc """
-	Likes of the currently logged in user (specified by the access_token)
+  @doc """
+  Likes of the currently logged in user (specified by the access_token)
 
-	See: https://developers.facebook.com/docs/graph-api/reference/user/likes
-	"""
-	@spec myLikes(access_token, options) :: response
-	def myLikes(access_token, options) do
-		fields = [access_token: access_token]
-		if !is_nil(Config.appsecret) do
-			fields = fields ++ [appsecret_proof: encrypt(access_token)]
-		end
-		Facebook.Graph.get("/me/likes", fields, options)
-	end
+  See: https://developers.facebook.com/docs/graph-api/reference/user/likes
+  """
+  @spec myLikes(access_token, options) :: response
+  def myLikes(access_token, options) do
+    fields = [access_token: access_token]
+    if !is_nil(Config.appsecret) do
+      fields = fields ++ [appsecret_proof: encrypt(access_token)]
+    end
+    Facebook.Graph.get("/me/likes", fields, options)
+  end
 
-	@doc """
-	Retrieves a list of granted permissions
+  @doc """
+  Retrieves a list of granted permissions
 
-	See: https://developers.facebook.com/docs/graph-api/reference/user/permissions
-	"""
-	@spec permissions(user_id :: integer | String.t, access_token) :: response
-	def permissions(user_id, access_token) do
-		permissions(user_id, access_token, [])
-	end
+  See: https://developers.facebook.com/docs/graph-api/reference/user/permissions
+  """
+  @spec permissions(user_id :: integer | String.t, access_token) :: response
+  def permissions(user_id, access_token) do
+    permissions(user_id, access_token, [])
+  end
 
-	@doc """
-	Retrieves a list of granted permissions
+  @doc """
+  Retrieves a list of granted permissions
 
-	See: https://developers.facebook.com/docs/graph-api/reference/user/permissions
-	"""
-	@spec permissions(user_id :: integer | String.t, access_token, options) :: response
-	def permissions(user_id, access_token, options) do
-		fields = [access_token: access_token]
-		if !is_nil(Config.appsecret) do
-			fields = fields ++ [appsecret_proof: encrypt(access_token)]
-		end
-		Facebook.Graph.get(~s(/#{user_id}/permissions), fields, options)
-	end
+  See: https://developers.facebook.com/docs/graph-api/reference/user/permissions
+  """
+  @spec permissions(user_id :: integer | String.t, access_token, options) :: response
+  def permissions(user_id, access_token, options) do
+    fields = [access_token: access_token]
+    if !is_nil(Config.appsecret) do
+      fields = fields ++ [appsecret_proof: encrypt(access_token)]
+    end
+    Facebook.Graph.get(~s(/#{user_id}/permissions), fields, options)
+  end
 
-	@doc """
-	Get the number of likes for the provided page_id
-	"""
-	@spec pageLikes(page_id :: integer | String.t, access_token) :: integer
-	def pageLikes(page_id, access_token) do
-		{:json, %{"likes" => likes}} = page(page_id, access_token, ["likes"], [])
-		likes
-	end
+  @doc """
+  Get the number of likes for the provided page_id
+  """
+  @spec pageLikes(page_id :: integer | String.t, access_token) :: integer
+  def pageLikes(page_id, access_token) do
+    {:json, %{"likes" => likes}} = page(page_id, access_token, ["likes"], [])
+    likes
+  end
 
-	@doc """
-	Basic page information for the provided page_id
+  @doc """
+  Basic page information for the provided page_id
 
-	See: https://developers.facebook.com/docs/graph-api/reference/page
-	"""
-	@spec page(page_id :: integer | String.t, access_token) :: response
-	def page(page_id, access_token) do
-		page(page_id, access_token, [], [])
-	end
+  See: https://developers.facebook.com/docs/graph-api/reference/page
+  """
+  @spec page(page_id :: integer | String.t, access_token) :: response
+  def page(page_id, access_token) do
+    page(page_id, access_token, [], [])
+  end
 
-	@doc """
-	Get page information for the specified fields for the provided page_id
+  @doc """
+  Get page information for the specified fields for the provided page_id
 
-	See: https://developers.facebook.com/docs/graph-api/reference/page
-	"""
-	@spec page(page_id :: integer | String.t, access_token, fields, options) :: response
-	def page(page_id, access_token, fields, options) do
-		params = [fields: fields, access_token: access_token]
-		if !is_nil(Config.appsecret) do
-			params = params ++ [appsecret_proof: encrypt(access_token)]
-		end
-		Facebook.Graph.get(~s(/#{page_id}), params, options)
-	end
+  See: https://developers.facebook.com/docs/graph-api/reference/page
+  """
+  @spec page(page_id :: integer | String.t, access_token, fields, options) :: response
+  def page(page_id, access_token, fields, options) do
+    params = [fields: fields, access_token: access_token]
+    if !is_nil(Config.appsecret) do
+      params = params ++ [appsecret_proof: encrypt(access_token)]
+    end
+    Facebook.Graph.get(~s(/#{page_id}), params, options)
+  end
 
 
-	defp encrypt(token) do
-		:hmac.hexlify(:crypto.hmac(:sha256, Config.appsecret, token), [:string, :lower])
-	end
+  defp encrypt(token) do
+    :hmac.hexlify(:crypto.hmac(:sha256, Config.appsecret, token), [:string, :lower])
+  end
 end
--- mix.exs
+++ mix.exs
@@ -1,68 +1,68 @@
 Code.ensure_loaded?(Hex) and Hex.start
 
 defmodule Facebook.Mixfile do
-	use Mix.Project
+  use Mix.Project
 
-	def project do
-		[
-			app: :facebook,
-			version: "0.4.0",
-			elixir: "~> 1.0",
-			description: description,
-			package: package,
-			deps: deps,
-			source_url: "https://github.com/mweibel/facebook.ex"
-		]
-	end
+  def project do
+    [
+      app: :facebook,
+      version: "0.4.0",
+      elixir: "~> 1.0",
+      description: description,
+      package: package,
+      deps: deps,
+      source_url: "https://github.com/mweibel/facebook.ex"
+    ]
+  end
 
-	# Configuration for the OTP application
-	def application do
-		[
-			mod: { Facebook, [] },
-			applications: [:json, :hackney, :logger],
-			env: [
-				env: :dev,
-				graph_url: "https://graph.facebook.com/v2.0",
-				appsecret: nil
-			]
-		]
-	end
+  # Configuration for the OTP application
+  def application do
+    [
+      mod: { Facebook, [] },
+      applications: [:json, :hackney, :logger],
+      env: [
+        env: :dev,
+        graph_url: "https://graph.facebook.com/v2.0",
+        appsecret: nil
+      ]
+    ]
+  end
 
-	defp description do
-		"""
-		Facebook Graph API Wrapper written in Elixir.
-		Please note, this is very much a work in progress. Feel free to contribute using pull requests.
-		"""
-	end
+  defp description do
+    """
+    Facebook Graph API Wrapper written in Elixir.
+    Please note, this is very much a work in progress. Feel free to contribute using pull requests.
+    """
+  end
 
-	defp package do
-		[
-			licenses: ["MIT"],
-			links: %{
-				"GitHub" => "https://github.com/mweibel/facebook.ex"
-			},
-			contributors: [
-				"Michael Weibel",
-				"Garrett Amini",
-				"Jamie Winsor",
-				"Adam Solove"
-			]
-		]
-	end
+  defp package do
+    [
+      licenses: ["MIT"],
+      links: %{
+        "GitHub" => "https://github.com/mweibel/facebook.ex"
+      },
+      contributors: [
+        "Michael Weibel",
+        "Garrett Amini",
+        "Jamie Winsor",
+        "Adam Solove"
+      ]
+    ]
+  end
 
-	# Returns the list of dependencies in the format:
-	# { :foobar, git: "https://github.com/elixir-lang/foobar.git", tag: "0.1" }
-	#
-	# To specify particular versions, regardless of the tag, do:
-	# { :barbat, "~> 0.1", github: "elixir-lang/barbat" }
-	defp deps do
-		[
-			{:json, ">= 0.3.0"},
-			{:hackney, "~> 1.0"},
-			{:libex_config, ">= 0.1.0"},
-			{:erlsha2, github: "vinoski/erlsha2", tag: "2.1"},
-			{:earmark, "~> 0.1", only: :dev},
-			{:ex_doc, "~> 0.6", only: :dev}
-		]
-	end
+  # Returns the list of dependencies in the format:
+  # { :foobar, git: "https://github.com/elixir-lang/foobar.git", tag: "0.1" }
+  #
+  # To specify particular versions, regardless of the tag, do:
+  # { :barbat, "~> 0.1", github: "elixir-lang/barbat" }
+  defp deps do
+    [
+      {:json, ">= 0.3.3"},
+      {:hackney, "~> 1.0"},
+      {:libex_config, ">= 0.1.0"},
+      {:erlsha2, github: "vinoski/erlsha2", tag: "2.1"},
+      {:earmark, "~> 0.1", only: :dev},
+      {:ex_doc, "~> 0.6", only: :dev}
+    ]
+  end
 end
