3759690ca4b25bea4a45c4cf0793513b99f4052b
Michael Weibel <michael.weibel@gmail.com>
2016-05-08 15:06:15 +0200

    More docs, unify coding style

--- lib/facebook.ex
+++ lib/facebook.ex
@@ -31,152 +31,159 @@ defmodule Facebook do
   @type fields :: list
   @type access_token :: String.t
   @type response :: {:json, HashDict.t} | {:body, String.t}
-  @type options :: list
   @type using_appsecret :: boolean
 
   @doc """
   If you want to use an appsecret proof, pass it into set_appsecret:
-  Facebook.set_appsecret("appsecret")
+
+  ## Example
+      iex> Facebook.setAppsecret("appsecret")
 
   See: https://developers.facebook.com/docs/graph-api/securing-requests
   """
-  def set_appsecret(appsecret) do
+  def setAppsecret(appsecret) do
     Config.appsecret(appsecret)
   end
 
   @doc """
-  Basic user infos of the logged in user (specified by the access_token)
+  Basic user infos of the logged in user (specified by the access_token).
+
+  ## Example
+      iex> Facebook.me("id,first_name", "<Your Token>")
+
+  See: https://developers.facebook.com/docs/graph-api/reference/user/
+  """
+  @spec me(fields :: String.t, access_token) :: response
+  def me(fields, access_token) when is_binary(fields) do
+    me([fields: fields], access_token)
+  end
+
+  @doc """
+  Basic user infos of the logged in user (specified by the access_token).
+
+  ## Example
+      iex> Facebook.me([fields: "id,first_name"], "<Your Token>")
 
   See: https://developers.facebook.com/docs/graph-api/reference/user/
   """
   @spec me(fields, access_token) :: response
-  def me(fields, access_token) when is_binary(fields) do
-    me([fields: fields], access_token, [])
-  end
-
   def me(fields, access_token) do
-    me(fields, access_token, [])
-  end
-
-  @doc """
-  Basic user infos of the logged in user (specified by the access_token).
-
-  See: https://developers.facebook.com/docs/graph-api/reference/user/
-  """
-  @spec me(fields :: String.t, access_token, options) :: response
-  def me(fields, access_token, options) when is_binary(fields) do
-    me([fields: fields], access_token, options)
-  end
-
-  @doc """
-  Basic user infos of the logged in user (specified by the access_token).
-
-  See: https://developers.facebook.com/docs/graph-api/reference/user/
-  """
-  @spec me(fields, access_token, options) :: response
-  def me(fields, access_token, options) do
     if !is_nil(Config.appsecret) do
       fields = fields ++ [appsecret_proof: encrypt(access_token)]
     end
 
-    Facebook.Graph.get("/me", fields ++ [access_token: access_token], options)
+    Facebook.Graph.get("/me", fields ++ [access_token: access_token])
   end
 
   @doc """
   A Picture for a Facebook User
 
+  ## Example
+      iex> Facebook.picture("<Some Id>", "small", "<Your Token>")
+
   See: https://developers.facebook.com/docs/graph-api/reference/user/picture/
   """
-  @spec picture(user_id :: String.t, type :: String.t, access_token, options) :: response
-  def picture(user_id, type, access_token, options \\ []) do
+  @spec picture(user_id :: String.t, type :: String.t, access_token) :: response
+  def picture(user_id, type, access_token) do
     fields = [type: type, redirect: false, access_token: access_token]
 
     if !is_nil(Config.appsecret) do
       fields = fields ++ [appsecret_proof: encrypt(access_token)]
     end
 
-    Facebook.Graph.get("/#{user_id}/picture", fields, options)
+    Facebook.Graph.get("/#{user_id}/picture", fields)
   end
 
   @doc """
   Likes of the currently logged in user (specified by the access_token)
+
+  ## Example
+      iex> Facebook.myLikes("<Your Token>")
 
   See: https://developers.facebook.com/docs/graph-api/reference/user/likes
   """
   @spec myLikes(access_token) :: response
   def myLikes(access_token) do
-    myLikes(access_token, [])
-  end
-
-  @doc """
-  Likes of the currently logged in user (specified by the access_token)
-
-  See: https://developers.facebook.com/docs/graph-api/reference/user/likes
-  """
-  @spec myLikes(access_token, options) :: response
-  def myLikes(access_token, options) do
     fields = [access_token: access_token]
     if !is_nil(Config.appsecret) do
       fields = fields ++ [appsecret_proof: encrypt(access_token)]
     end
-    Facebook.Graph.get("/me/likes", fields, options)
+    Facebook.Graph.get("/me/likes", fields)
   end
 
   @doc """
   Retrieves a list of granted permissions
+
+  ## Example
+      iex> Facebook.permissions("<Some Id>", "<Your Token>")
 
   See: https://developers.facebook.com/docs/graph-api/reference/user/permissions
   """
   @spec permissions(user_id :: integer | String.t, access_token) :: response
   def permissions(user_id, access_token) do
-    permissions(user_id, access_token, [])
-  end
-
-  @doc """
-  Retrieves a list of granted permissions
-
-  See: https://developers.facebook.com/docs/graph-api/reference/user/permissions
-  """
-  @spec permissions(user_id :: integer | String.t, access_token, options) :: response
-  def permissions(user_id, access_token, options) do
     fields = [access_token: access_token]
     if !is_nil(Config.appsecret) do
       fields = fields ++ [appsecret_proof: encrypt(access_token)]
     end
-    Facebook.Graph.get(~s(/#{user_id}/permissions), fields, options)
+    Facebook.Graph.get(~s(/#{user_id}/permissions), fields)
   end
 
   @doc """
-  Get the number of likes for the provided page_id
+  Get the count of fans for the provided page_id
+
+  ## Example
+      iex> Facebook.fanCount("CocaColaMx", "<Your Token>")
+
+  See: https://developers.facebook.com/docs/graph-api/reference/page/
   """
-  @spec pageLikes(page_id :: integer | String.t, access_token) :: integer
+  @spec fanCount(page_id :: integer | String.t, access_token) :: integer
+  def fanCount(page_id, access_token) do
+    {:json, %{"fan_count" => fanCount}} = page(page_id, access_token, ["fan_count"])
+    fanCount
+  end
+
+  @doc """
+  *Deprecated:* Please use fanCount instead.
+
+  Get the count of fans for the provided page_id
+
+  ## Example
+      iex> Facebook.pageLikes("CocaColaMx", "<Your Token>")
+
+  See: https://developers.facebook.com/docs/graph-api/reference/page/
+  """
   def pageLikes(page_id, access_token) do
-    {:json, %{"likes" => likes}} = page(page_id, access_token, ["likes"], [])
-    likes
+    fanCount(page_id, access_token)
   end
 
   @doc """
   Basic page information for the provided page_id
 
+  ## Example
+      iex> Facebook.page("CocaColaMx", "<Your Token>")
+
   See: https://developers.facebook.com/docs/graph-api/reference/page
   """
   @spec page(page_id :: integer | String.t, access_token) :: response
   def page(page_id, access_token) do
-    page(page_id, access_token, [], [])
+    page(page_id, access_token, [])
   end
 
   @doc """
   Get page information for the specified fields for the provided page_id
 
+  ## Example
+      iex> Facebook.page("CocaColaMx", "<Your Token>", "id")
+
   See: https://developers.facebook.com/docs/graph-api/reference/page
   """
-  @spec page(page_id :: integer | String.t, access_token, fields, options) :: response
-  def page(page_id, access_token, fields, options) do
+  @spec page(page_id :: integer | String.t, access_token, fields) :: response
+  def page(page_id, access_token, fields) do
     params = [fields: fields, access_token: access_token]
     if !is_nil(Config.appsecret) do
       params = params ++ [appsecret_proof: encrypt(access_token)]
     end
-    Facebook.Graph.get(~s(/#{page_id}), params, options)
+    Facebook.Graph.get(~s(/#{page_id}), params)
   end
 
   @doc """
