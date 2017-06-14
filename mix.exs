69d2fed9d92d1d5e357bcd295db8675b3f07844e
Michael Weibel <michael.weibel@gmail.com>
2017-06-14 18:55:34 +0200

    Version 0.13.2

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule Facebook.Mixfile do
   def project do
     [
       app: :facebook,
-      version: "0.13.1",
+      version: "0.13.2",
       elixir: "~> 1.0",
       description: description(),
       package: package(),
