0499aceb709403fe48b1b71498541c29ee843d9c
Matteo Centenaro <bugant@gmail.com>
2018-08-30 17:42:51 +0200

    NEW: optionally configure connection timeouts
    Optionally configure connection and receive timeout for network requests
    via two new config options: `request_conn_timeout` and
    `request_recv_timeout`.

--- lib/facebook/graph_api.ex
+++ lib/facebook/graph_api.ex
@@ -5,6 +5,18 @@ defmodule Facebook.GraphAPI do
 
   alias Facebook.Config
 
+  def process_request_options(options) do
+    updated_options = case Config.request_conn_timeout do
+      :error -> options
+      val -> options ++ [timeout: val]
+    end
+
+    case Config.request_recv_timeout do
+      :error -> updated_options
+      val -> updated_options ++ [recv_timeout: val]
+    end
+  end
+
   def process_url(url), do: Config.graph_url <> url
 
   def process_response_body(body) do
