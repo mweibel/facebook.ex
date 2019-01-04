9587ed3f2dcd50f281858edffcc126397223461f
Mickey <poverwhelming@gmail.com>
2019-01-04 13:05:05 +0800

    ran mix format on GraphAPI

--- lib/facebook/graph_api.ex
+++ lib/facebook/graph_api.ex
@@ -6,12 +6,13 @@ defmodule Facebook.GraphAPI do
   alias Facebook.Config
 
   def process_request_options(options) do
-    updated_options = case Config.request_conn_timeout do
-      :error -> options
-      val -> options ++ [timeout: val]
-    end
+    updated_options =
+      case Config.request_conn_timeout() do
+        :error -> options
+        val -> options ++ [timeout: val]
+      end
 
-    case Config.request_recv_timeout do
+    case Config.request_recv_timeout() do
       :error -> updated_options
       val -> updated_options ++ [recv_timeout: val]
     end
@@ -19,10 +20,10 @@ defmodule Facebook.GraphAPI do
 
   def process_url("https://graph.facebook.com/" <> _ = url), do: url
 
-  def process_url(url), do: Config.graph_url <> url
+  def process_url(url), do: Config.graph_url() <> url
 
   def process_response_body(body) do
     body
-      |> JSON.decode
+    |> JSON.decode()
   end
 end
