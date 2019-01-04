88fea41190f68b9805b73aba686fffe742cab599
Mickey <yuchunc@users.noreply.github.com>
2019-01-04 17:02:41 +0800

    Use the exact url in the pagination response (#73)
    * use the original url if it already contains the domain info
    * ran mix format on GraphAPI
    * patteren match on the protocol instead of the full domain

--- lib/facebook/graph_api.ex
+++ lib/facebook/graph_api.ex
@@ -6,21 +6,24 @@ defmodule Facebook.GraphAPI do
   alias Facebook.Config
 
   def process_request_options(options) do
-    updated_options = case Config.request_conn_timeout do
-      :error -> options
-      val -> options ++ [timeout: val]
-    end
+    updated_options =
+      case Config.request_conn_timeout() do
+        :error -> options
+        val -> options ++ [timeout: val]
+      end
 
-    case Config.request_recv_timeout do
+    case Config.request_recv_timeout() do
       :error -> updated_options
       val -> updated_options ++ [recv_timeout: val]
     end
   end
 
-  def process_url(url), do: Config.graph_url <> url
+  def process_url("https://" <> _ = url), do: url
+
+  def process_url(url), do: Config.graph_url() <> url
 
   def process_response_body(body) do
     body
-      |> JSON.decode
+    |> JSON.decode()
   end
 end
