fa7024c11f2c4ab88111e8e58b546010dad227f4
heitor <heitor.polidoro@gmail.com>
2021-04-07 10:06:28 -0300

    missing graph video api config

--- lib/facebook/graph_video_api.ex
+++ lib/facebook/graph_video_api.ex
@@ -5,6 +5,21 @@ defmodule Facebook.GraphVideoAPI do
 
   alias Facebook.Config
 
+  def process_request_options(options) do
+    updated_options =
+      case Config.request_conn_timeout() do
+        nil -> options
+        val -> options ++ [timeout: val]
+      end
+
+    case Config.request_recv_timeout() do
+      nil -> updated_options
+      val -> updated_options ++ [recv_timeout: val]
+    end
+  end
+
+  def process_url("https://" <> _ = url), do: url
+
   def process_url(url), do: Config.graph_video_url() <> url
 
   def process_response_body(body), do: JSON.decode(body)
