2980502ecee4123f711630349d814a57e61b627f
Copser <ppilipovic84@gmail.com>
2021-10-02 06:14:13 +0200

    replace app id and app secret

--- test/facebook_test.exs
+++ test/facebook_test.exs
@@ -15,8 +15,8 @@ defmodule FacebookTest do
   # HTTPoison to do it's thing.
   #
 
-  @app_id "123"
-  @app_secret "456"
+  @app_id "1661954077415306"
+  @app_secret "f77497e925c46e8a4c241fff8a03e0ec"
 
   # This is the facebook for developers page id
   @page_id 19_292_868_552
